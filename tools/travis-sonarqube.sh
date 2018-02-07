1f6e3e270b7452729adc765ef3e6bfd84ed8025f
Thomas Epperson <thomas.epperson@gmail.com>
2018-02-07 07:22:40 -0500

    Remove unused portions of travis-sonarqube.sh and actually call it from the travis config

--- tools/travis-sonarqube.sh
+++ tools/travis-sonarqube.sh
@@ -1,9 +1,5 @@
 #! /bin/sh
 
-# Install and run SonarQube on travis. 
-#
-# Use it as a wrapper to your build command, eg: ./travis-sonarqube.sh make VERBOSE=1
-
 # On Mac OSX, you don't want to run SonarQube but to exec the build command directly.
 if [ ${TRAVIS_OS_NAME} != 'linux' ]  
 then
@@ -11,25 +7,8 @@ then
 fi
 # Passed this point, we are on Linux (exec never returns)
 
-
 # Be verbose and fail fast
 set -ex
-
-# Install required software
-installSonarQubeScanner() {
-  export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-2.6
-  rm -rf $SONAR_SCANNER_HOME
-  mkdir -p $SONAR_SCANNER_HOME
-  curl -sSLo $HOME/.sonar/sonar-scanner.zip http://repo1.maven.org/maven2/org/sonarsource/scanner/cli/sonar-scanner-cli/2.6/sonar-scanner-cli-2.6.zip
-  unzip $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
-  rm $HOME/.sonar/sonar-scanner.zip
-  export PATH=$SONAR_SCANNER_HOME/bin:$PATH
-  export SONAR_SCANNER_OPTS="-server"
-}
-installBuildWrapper() {
-  curl -LsS https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip > build-wrapper-linux-x86.zip
-  unzip build-wrapper-linux-x86.zip
-}
 
 #get the current git branch name
 branch_name=$(git rev-parse --abbrev-ref HEAD)
@@ -37,13 +16,4 @@ if [ "$branch_name" != "master" ]
 then
 	cat "\nsonar.branch.name=$branch_name \n" >> ./sonar-project.properties
 fi
-
-installSonarQubeScanner
-installBuildWrapper
-
-# triggers the compilation through the build wrapper to gather compilation database
-./build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir wrap "$@"
-
-# and finally execute the actual SonarQube analysis (the SONAR_TOKEN is set from the travis web interface, to not expose it)
-sonar-scanner -Dsonar.host.url=https://sonarqube.com -Dsonar.login=$SONARQUBE_KEY
 
