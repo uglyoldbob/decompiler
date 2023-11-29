16aac05d242ad154aaee7f4a1ce5154923eff757
Thomas Epperson <thomas.epperson@gmail.com>
2023-11-29 12:29:20 -0600

    Some progress on the graph generator.

--- src/graph.rs
+++ src/graph.rs
@@ -14,10 +14,54 @@ use egui_multiwin::enum_dispatch::enum_dispatch;
 use egui_multiwin_dynamic::multi_window::NewWindowRequest;
 use egui_multiwin_dynamic::tracked_window::RedrawResponse;
 use egui_multiwin_dynamic::tracked_window::TrackedWindow;
+use graphviz_rust::printer::PrinterContext;
 use std::sync::Arc;
+
+use graphviz_rust::dot_generator::*;
+use graphviz_rust::dot_structures::Graph;
+use graphviz_rust::dot_structures::*;
 
 pub struct AppCommon {
     clicks: u32,
+}
+
+pub struct GraphIterator {
+    gg: GraphGenerator,
+    u: u32,
+}
+
+impl Iterator for GraphIterator {
+    type Item = Graph;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut g = graph!(di id!("generated"));
+        for i in 0..self.gg.num_blocks {
+            let s = format!("{}", i);
+            let id = graphviz_rust::dot_structures::Id::Plain(s);
+            let id = graphviz_rust::dot_structures::NodeId(id, None);
+            let e = graphviz_rust::dot_structures::Node {
+                id,
+                attributes: vec![],
+            };
+            g.add_stmt(graphviz_rust::dot_structures::Stmt::Node(e));
+        }
+        Some(g)
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct GraphGenerator {
+    num_blocks: u32,
+}
+
+impl GraphGenerator {
+    pub fn new(num_blocks: u32) -> Self {
+        Self { num_blocks }
+    }
+
+    pub fn create_iter(&self) -> GraphIterator {
+        GraphIterator { gg: *self, u: 42 }
+    }
 }
 
 #[derive(Debug)]
@@ -32,12 +76,36 @@ impl CustomEvent {
     }
 }
 
-pub struct RootWindow {}
+pub struct RootWindow {
+    done: bool,
+    generated: bool,
+    redraw: bool,
+    image_is_recent: bool,
+    gg: GraphGenerator,
+    gi: GraphIterator,
+    graph: Graph,
+    png: Vec<u8>,
+    texture: Option<egui_multiwin::egui::TextureHandle>,
+}
 
 impl RootWindow {
     pub fn request() -> NewWindowRequest {
+        let gg = GraphGenerator::new(3);
+        let mut gi = gg.create_iter();
+        let graph = gi.next().unwrap();
+
         NewWindowRequest {
-            window_state: MyWindows::Root(RootWindow {}),
+            window_state: MyWindows::Root(RootWindow {
+                done: false,
+                generated: true,
+                image_is_recent: false,
+                redraw: false,
+                gg,
+                gi,
+                graph,
+                png: vec![],
+                texture: None,
+            }),
             builder: egui_multiwin::winit::window::WindowBuilder::new()
                 .with_resizable(false)
                 .with_inner_size(egui_multiwin::winit::dpi::LogicalSize {
@@ -67,8 +135,106 @@ impl TrackedWindow for RootWindow {
         _clipboard: &mut arboard::Clipboard,
     ) -> RedrawResponse {
         let quit = false;
+
+        if !self.generated {
+            if let Some(g) = self.gi.next() {
+                self.graph = g;
+                self.image_is_recent = false;
+            } else {
+                self.done = true;
+            }
+            self.generated = true;
+        }
+
+        if !self.image_is_recent {
+            let graph_png = graphviz_rust::exec(
+                self.graph.clone(),
+                &mut PrinterContext::default(),
+                vec![graphviz_rust::cmd::Format::Png.into()],
+            )
+            .unwrap()
+            
+            .as_bytes()
+            .to_vec();
+            self.png = graph_png;
+            self.redraw = true;
+            self.image_is_recent = true;
+        }
+
+        if self.redraw {
+            println!("Image buf size is {}", self.png.len());
+            std::fs::write("./generated.png", &self.png).unwrap();
+            let mut decoder = zune_png::PngDecoder::new(&self.png);
+            let (w, h) = decoder.get_dimensions().unwrap();
+            let p = decoder.decode().unwrap();
+
+            let pixels: Vec<egui_multiwin::egui::Color32> = match p {
+                zune_png::zune_core::result::DecodingResult::U8(img) => img
+                    .chunks_exact(3)
+                    .map(|raw| egui_multiwin::egui::Color32::from_rgb(raw[0], raw[1], raw[2]))
+                    .collect(),
+                zune_png::zune_core::result::DecodingResult::U16(img) => img
+                    .chunks_exact(3)
+                    .map(|raw| {
+                        egui_multiwin::egui::Color32::from_rgb(
+                            (raw[0] >> 8) as u8,
+                            (raw[1] >> 8) as u8,
+                            (raw[2] >> 8) as u8,
+                        )
+                    })
+                    .collect(),
+                zune_png::zune_core::result::DecodingResult::F32(img) => img
+                    .chunks_exact(3)
+                    .map(|raw| {
+                        egui_multiwin::egui::Color32::from_rgb(
+                            (raw[0] * 255.0) as u8,
+                            (raw[1] * 255.0) as u8,
+                            (raw[2] * 255.0) as u8,
+                        )
+                    })
+                    .collect(),
+                _ => {
+                    todo!();
+                }
+            };
+
+            let image = egui_multiwin::egui::ColorImage {
+                size: [w, h],
+                pixels,
+            };
+
+            if self.texture.is_none() {
+                self.texture = Some(egui.egui_ctx.load_texture(
+                    "graph",
+                    image,
+                    egui_multiwin::egui::TextureOptions::NEAREST,
+                ));
+            } else if let Some(t) = &mut self.texture {
+                if t.size()[0] != image.width() || t.size()[1] != image.height() {
+                    self.texture = Some(egui.egui_ctx.load_texture(
+                        "graph",
+                        image,
+                        egui_multiwin::egui::TextureOptions::NEAREST,
+                    ));
+                } else {
+                    t.set_partial([0, 0], image, egui_multiwin::egui::TextureOptions::NEAREST);
+                }
+            }
+        }
+
         egui_multiwin::egui::CentralPanel::default().show(&egui.egui_ctx, |ui| {
             ui.heading(format!("number {}", c.clicks));
+            if let Some(t) = &self.texture {
+                ui.add(egui_multiwin::egui::Image::from_texture(
+                    egui_multiwin::egui::load::SizedTexture {
+                        id: t.id(),
+                        size: egui_multiwin::egui::Vec2 {
+                            x: t.size()[0] as f32,
+                            y: t.size()[1] as f32,
+                        },
+                    },
+                ));
+            }
         });
         RedrawResponse {
             quit,
@@ -78,9 +244,8 @@ impl TrackedWindow for RootWindow {
 }
 
 impl AppCommon {
-    fn process_event(&mut self, event: CustomEvent) -> Vec<NewWindowRequest> {
-        let mut windows_to_create = vec![];
-        windows_to_create
+    fn process_event(&mut self, _event: CustomEvent) -> Vec<NewWindowRequest> {
+        vec![]
     }
 }
 
